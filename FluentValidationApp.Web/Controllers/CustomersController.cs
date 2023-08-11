ccd2eca124e8cfab45fc538e4022b3b765646816
pelinile <nilpelinn1@gmail.com>
2023-08-11 15:19:12 +0300

    Add project files.

---
+++ FluentValidationApp.Web/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidationApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidationApp.Web.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+
+                return NotFound();
+            }
+            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.Id == id);
+            return View(customer);
+        }
+    }
+}
